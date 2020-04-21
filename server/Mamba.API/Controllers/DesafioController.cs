9582f6494fea8610e8c2364edb5e660170de5cf5
Victor Savoi <savoi.victor@hotmail.com>
2020-04-21 18:04:29 -0300

    Serviço de edição para desafio na API

--- server/Mamba.API/Controllers/DesafioController.cs
+++ server/Mamba.API/Controllers/DesafioController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using AutoMapper;
 using Mamba.API.Model;
 using Mamba.Application.Interface;
 using Mamba.Domain.Entities;
@@ -17,14 +19,15 @@ namespace Mamba.API.Controllers
         private readonly IDesafioAppService _desafioAppService;
         private readonly IQuestaoAppService _questaoAppService;
         private readonly IEmpresaAppService _empresaAppService;
+        private readonly IMapper _mapper;
 
-        public DesafioController(IDesafioAppService desafioAppService, IQuestaoAppService questaoAppService, IEmpresaAppService empresaAppService)
+        public DesafioController(IDesafioAppService desafioAppService, IQuestaoAppService questaoAppService, IEmpresaAppService empresaAppService, IMapper mapper)
         {
             _desafioAppService = desafioAppService;
             _questaoAppService = questaoAppService;
             _empresaAppService = empresaAppService;
+            _mapper = mapper;
         }
-
 
         /// <summary>
         /// Lista todos os desafios registrados
@@ -32,6 +35,13 @@ namespace Mamba.API.Controllers
         /// <returns>Lista dos desafios registrados</returns>
         /// <response code="200">Retorna os desafios registrados</response>
         /// <response code="400">Erro ao buscar os desafios</response>
+        /// <remarks>
+        /// Orientações de testes:
+        /// 
+        ///     Clique no botão "Try it out" abaixo e depois no botão "Execute" que será exibido para testar o serviço.
+        ///     
+        ///     Dica: Utilize este serviço para ver os Id's disponíveis para os próximos testes.
+        /// </remarks>
         [HttpGet]
         public async Task<IActionResult> ListarDesafios()
         {
@@ -50,11 +60,19 @@ namespace Mamba.API.Controllers
         /// <response code="200">Retorna o desafio encontrado</response>
         /// <response code="400">Erro ao buscar um desafio</response>
         /// <response code="404">Desafio não encontrado</response>
+        /// <remarks>
+        /// Orientações de testes:
+        /// 
+        ///     Clique no botão "Try it out" abaixo, informe o id de um desafio cadastrado e depois no botão "Execute" para testar o serviço.
+        /// 
+        ///     Dica: Utilize o serviço de listagem para encontrar um id para o teste.
+        /// </remarks>
         [HttpGet]
         [Route("{id:int}")]
         public async Task<IActionResult> BuscarDesafioPorId(int id)
         {
             Desafio desafio = _desafioAppService.GetById(id);
+            if (desafio == null) return NotFound();
 
             return Ok(new DesafioModel
             {
@@ -71,6 +89,13 @@ namespace Mamba.API.Controllers
         /// <response code="200">Desafio excluído com sucesso</response>
         /// <response code="400">Erro ao excluir o desafio</response>
         /// <response code="404">Desafio não encontrado</response>
+        /// <remarks>
+        /// Orientações de testes:
+        /// 
+        ///     Clique no botão "Try it out" abaixo, informe o id de um desafio cadastrado e depois no botão "Execute" para testar o serviço.
+        /// 
+        ///     Dica: Utilize o serviço de listagem para encontrar um id para o teste.
+        /// </remarks>
         [HttpDelete]
         [Route("excluir/{id:int}")]
         public async Task<IActionResult> ExcluirDesafio(int id)
@@ -85,6 +110,10 @@ namespace Mamba.API.Controllers
 
                     _desafioAppService.Remove(desafio);
                 }
+                else
+                {
+                    return NotFound();
+                }
 
                 return Ok();
             }
@@ -95,13 +124,17 @@ namespace Mamba.API.Controllers
         }
 
         /// <summary>
-        /// Registra/Atualiza um desafio
+        /// Registra um novo desafio
         /// </summary>
         /// <param name="model"></param>
         /// <returns></returns>
-        /// <response code="200">Desafio registrado/atualizado com sucesso</response>
-        /// <response code="400">Erro ao registrar/atualizado novo desafio</response>
-        /// <response code="404">Desafio não encontrado</response>
+        /// <response code="200">Desafio registrado com sucesso</response>
+        /// <response code="400">Erro ao registrar novo desafio</response>
+        /// <remarks>
+        /// Orientações de testes:
+        /// 
+        ///     Clique no botão "Try it out" abaixo, informe todos os campos exigidos e depois no botão "Execute" para testar o serviço.
+        /// </remarks>
         [HttpPost]
         public async Task<IActionResult> Salvar(DesafioAddModel model)
         {
@@ -140,6 +173,42 @@ namespace Mamba.API.Controllers
                 }
 
                 return Ok();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Atualiza um desafio registrado
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        /// <response code="200">Desafio atualizado com sucesso</response>
+        /// <response code="400">Erro ao atualizar desafio</response>
+        /// <response code="404">Desafio não encontrado</response>
+        [HttpPut]
+        public async Task<IActionResult> Editar(DesafioAddModel model)
+        {
+            try
+            {
+                if (model.IdDesafio > 0)
+                {
+                    Desafio desafio = _desafioAppService.GetById(model.IdDesafio);
+                    desafio.DataUltimaAlteracao = DateTime.Now;
+                    desafio.ProcessoCadastro = "EmpresaController.Editar";
+
+                    desafio = _mapper.Map<Desafio>(model);
+
+                    _desafioAppService.Update(desafio);
+
+                    return Ok();
+                }
+                else
+                {
+                    return NotFound();
+                }
             }
             catch (Exception ex)
             {
