aed6fa53d0ee2bf85f1f9f6a8ff9bbc36f8d8671
Victor Savoi <savoi.victor@hotmail.com>
2020-04-22 21:28:12 -0300

    Correção no link do enviroment

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://mambafiap.azurewebsites.net/',
+  apiUrl: 'http://mambafiap.azurewebsites.net',
 };
--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://mambafiap.azurewebsites.net/',
+  apiUrl: 'http://mambafiap.azurewebsites.net',
 };
 
 /*
