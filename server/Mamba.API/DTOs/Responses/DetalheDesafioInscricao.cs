be559f947fb23ea6e46d99897323de87b41b286d
Victor Savoi <savoi.victor@hotmail.com>
2020-11-11 21:11:30 -0300

    Novo endpoit anonimo para os detalhes da vaga para uma nova inscrição

---
+++ server/Mamba.API/DTOs/Responses/DetalheDesafioInscricao.cs
@@ -0,0 +1,40 @@
+﻿using Mamba.API.Extensions.Utilities;
+using System;
+
+namespace Mamba.API.DTOs.Responses
+{
+    public class DetalheDesafioInscricao
+    {
+        public string Cargo { get; set; }
+        public DateTime DataAbertura { get; set; }
+        public DateTime? DataFechamento { get; set; }
+
+        public string StatusDesafio
+        {
+            get
+            {
+                if (!DataFechamento.HasValue)
+                    return "Aberto";
+
+                if (DataFechamento.Value > DateTime.Now.Date)
+                    return "Aberto";
+
+                return "Fechado";
+            }
+        }
+
+        public string TempoStatus
+        {
+            get
+            {
+                if (DataFechamento.HasValue)
+                {
+                    if (DataFechamento.Value <= DateTime.Now)
+                        return DateTimeUtilities.ObterPeriodoTemporal(DataFechamento.Value, prefixo: "Fechado");
+                }
+
+                return DateTimeUtilities.ObterPeriodoTemporal(DataAbertura, prefixo: "Aberto");
+            }
+        }
+    }
+}
